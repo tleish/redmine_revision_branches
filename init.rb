2424a61b8ad5d3cd0b08a42f0bc08023ef525e98
Thomas Leishman <thomas.leishman@plansource.com>
2017-07-22 09:43:57 -0600

    Fix for #18 to support Redmine 3.4.1 and it's change in Plugin Settings Hash from symbol to string

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_revision_branches do
   name 'Revision Branches'
   author 'Thomas Leishman'
   description 'The Redmine Revision Branches plugin adds branch information to the revisions page, specifically for git.'
-  version '0.3.1'
+  version '0.3.2'
   url 'https://github.com/tleish/redmine_revision_branches/'
   author_url 'https://github.com/tleish'
   settings :default => { display_under_single_revision: true, display_under_associated_revisions: false }, :partial => 'redmine_revision_branches/settings'
